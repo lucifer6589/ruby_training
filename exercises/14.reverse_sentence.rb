0d7d80ace32ef3aa49a70042e3f0f9bdfac5533a
Mohammad Kaif <mohammadkaif6589@gmail.com>
2026-08-08 10:36:18 +0530

    exercise 14 : corrected almost all RuboCop error

--- exercises/14.reverse_sentence.rb
+++ exercises/14.reverse_sentence.rb
@@ -1,18 +1,15 @@
-#Use string methods to reverse the words arrangement in a sentence.
-#Eg: "An apple a day keeps the doctor away" -> "away doctor the keeps day a apple An"
+# frozen_string_literal: true
+
 def reverse_sentence(sentence)
   words = sentence.split
   reversed_words = words.reverse
-  reversed_words.join(" ")
+  reversed_words.join(' ')
 end
 
 sentence = ARGV[0]
 
 if sentence.nil? || sentence.strip.empty?
-  puts "Please provide an input"
-else 
+  puts 'Please provide an input'
+else
   p reverse_sentence(sentence.strip)
 end
-
-
-
